ec69a5d87ab69e7c0e41f4e7c2e639727d4e682c
Johnny Borg <johnny@e-active.nl>
2017-03-09 13:45:16 +0100

    added login step

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -10,11 +10,46 @@
  */
 
 use Behat\Behat\Context\Context;
+use Behat\Mink\Driver\BrowserKitDriver;
+use Behat\Mink\Exception\UnsupportedDriverActionException;
 use Behat\MinkExtension\Context\MinkContext;
+
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 /**
  * @author Johnny Borg <johnny@e-active.nl>
  */
 class FeatureContext extends MinkContext implements Context
 {
+    /**
+     * @Given /^I am authenticated as "([^"]*)"$/
+     * @param string $username
+     * @throws UnsupportedDriverActionException
+     */
+    public function iAmAuthenticatedAs($username)
+    {
+        $driver = $this->getSession()->getDriver();
+        if (!$driver instanceof BrowserKitDriver) {
+            throw new UnsupportedDriverActionException(
+                'This step is only supported by the BrowserKitDriver (given: $s)',
+                $driver
+            );
+        }
+
+        /** @var \Symfony\Bundle\FrameworkBundle\Client $client */
+        $client = $driver->getClient();
+        $client->getCookieJar()->set(new Cookie(session_name(), true));
+
+        $session = $client->getContainer()->get('session');
+
+        $user = $client->getContainer()->get('integrated_user.user.manager')->findByUsername($username);
+
+        $token = new UsernamePasswordToken($user, null, 'default', $user->getRoles());
+        $session->set('_security_default', serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $client->getCookieJar()->set($cookie);
+    }
 }
---
+++ features/content.feature
@@ -0,0 +1,14 @@
+Feature: Security
+  In order to block access from anonymous users
+  As a user
+  I must login to proceed so i can edit content
+
+  Scenario: Create content
+    Given I am authenticated as "demo"
+      And I am on "/admin/content"
+    Then I should not see "Login"
+
+  Scenario: New article
+    Given I am authenticated as "demo"
+      And I am on "/admin/content"
+    When I follow ""
