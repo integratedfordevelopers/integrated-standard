06d665cdf7023707fbfa8ff32620b11460daf401
Marijn Otte <marijn@e-active.nl>
2017-03-21 13:40:37 +0100

    Include Thompson theme in default install

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -44,6 +44,7 @@ class AppKernel extends Kernel
             new Integrated\Bundle\ContentHistoryBundle\IntegratedContentHistoryBundle(),
             new Integrated\Bundle\ImageBundle\IntegratedImageBundle(),
             new Integrated\Bundle\CommentBundle\IntegratedCommentBundle(),
+            new Integrated\Bundle\ThompsonThemeBundle\IntegratedThompsonThemeBundle(),
 
             new AppBundle\AppBundle(),
         );
