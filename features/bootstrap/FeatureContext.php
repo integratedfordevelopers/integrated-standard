f6d8cef0315403b54c7d272091a455c50f38ea5b
Johnny Borg <johnny@e-active.nl>
2017-03-23 11:11:17 +0100

    added test for new article

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -11,6 +11,7 @@
 
 use Behat\Behat\Context\Context;
 use Behat\Mink\Driver\BrowserKitDriver;
+use Behat\Mink\Exception\ExpectationException;
 use Behat\Mink\Exception\UnsupportedDriverActionException;
 use Behat\MinkExtension\Context\MinkContext;
 
@@ -56,6 +57,45 @@ class FeatureContext extends MinkContext implements Context
         } else {
             throw new UsernameNotFoundException(
                 sprintf('The requested user %s is not found', $username)
+            );
+        }
+    }
+
+    /**
+     * @Given /^I am going to create a "([^"]*)"$/
+     * @param string $name
+     * @throws \Behat\Mink\Exception\ExpectationException
+     */
+    public function createNewContent($name)
+    {
+        $this->getSession()->visit('/admin/content');
+        $contentTypes = $this->getSession()->getPage()->findAll('xpath', '//div[@class="megamenu-content"]//li');
+
+        if (count($contentTypes)) {
+            // Put the content type here
+            $_element = null;
+
+            // Iterate the content types in the list
+            foreach ($contentTypes as $element) {
+                /** @var Behat\Mink\Element\NodeElement $element */
+                if (false !== strpos($element->getText(), $name)) {
+                    $_element = $element;
+                }
+            }
+
+            if (null === $_element) {
+                throw new ExpectationException(
+                    sprintf('Content type %s not found under "Create new". do you have the correct rights?', $name),
+                    $this->getSession()->getDriver()
+                );
+            }
+
+            $_element->clickLink($name);
+        } else {
+            // No navigation found
+            throw new ExpectationException(
+                'No content items found under "Create new", are you sure you are loggedin?',
+                $this->getSession()->getDriver()
             );
         }
     }
--- features/content.feature
+++ features/content.feature
@@ -1,6 +1,6 @@
-Feature: Security
-  In order to block access from anonymous users
-  As a user
+Feature: Content
+  In order to place content on my website i must have be able to work in the backend
+  As a publisher
   I must login to proceed so i can edit content
 
   Scenario: Create content
@@ -10,4 +10,8 @@ Feature: Security
 
   Scenario: New article
     Given I am authenticated as "demo"
-      And I am on "/admin/content"
+      And I am going to create a "Article"
+    When I fill in "integrated_content[title]" with "Integrated welcomes Behat"
+      And I fill in "integrated_content[intro]" with "Integrated will be functional tested with the awesome framework Behat."
+      And I press "Create"
+    Then I should see "The document Article has been created"
