e4fd1392d2aa7c209777cfe910322b5ef1e90e26
Koen Prins <koen_vs_prins@hotmail.com>
2017-07-12 16:20:35 +0200

    INTEGRATED-1190 slug migration

---
+++ app/Migrations/MongoDB/Version20170712125412.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Application\Migrations\MongoDB;
+
+use AppBundle\Doctrine\ODM\Migration\AbstractMigration;
+
+use Doctrine\MongoDB\Database;
+
+use Integrated\Bundle\ContentBundle\Document\Content\Content;
+
+/**
+ * @author Koen Prins <koen@e-active.nl>
+ */
+class Version20170712125412 extends AbstractMigration
+{
+    /**
+     * @param Database $db
+     */
+    public function up(Database $db)
+    {
+        $content = $this->getSluglessContent();
+
+        $count = 0;
+        foreach ($content as $item) {
+            $item->setSlug(false);
+            if ($count % 500 == 0) {
+                $this->getDocumentManager()->flush();
+            }
+            $count++;
+        }
+        $this->getDocumentManager()->flush();
+    }
+
+    /**
+     * @param Database $db
+     */
+    public function down(Database $db)
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getSluglessContent()
+    {
+        return $this->getDocumentManager()->createQueryBuilder(Content::class)
+            ->field('slug')->exists(false)
+            ->getQuery()
+            ->execute()
+            ->toArray();
+    }
+}
